65149996dff3fc53d986b7e047b2a6fecaf5257d
brownie <raisemmina@gmail.com>
2025-06-29 17:37:53 +0800

    initial implementation of tenant-related features

--- services/tenantService.py
+++ services/tenantService.py
@@ -1,13 +1,72 @@
 # Brownie
+from common import AESCipher
+from common import CIPHER_KEY
+from services import dataService
 
+def createTenant(appId, appSecret, tenantId, userlist):
+    cipher = AESCipher(CIPHER_KEY)
 
-def createTenant():
-    pass
+    hashedAppId = cipher.encrypt(appId)
+    hashedAppSecret = cipher.encrypt(appSecret)
+    tenantName = cipher.encrypt(tenantId)
 
+    if isinstance(userlist, dict):
+        userlist = [userlist]
 
-def getTenantInfo():
-    pass
+    dataService.createDB(tenantId, tenantName)
+    dataService.creatCollection('info', tenantName, _id='singleton',appId=hashedAppId, appSecret=hashedAppSecret)
+    dataService.creatCollection('users', tenantName, userlist=userlist)
 
+def _getTenantInfo(tenantName):
+    """ return json object"""
+    doc = dataService.getDocument('info', tenantName, _id='singleton')
+    return doc
 
-def getTenantUsers():
-    pass
+def getTenantAccessToken(tenantName):
+    """ get access token from db
+        return
+            1. access token string (already decrypted)
+            2. None, if not exist"""
+    info = _getTenantInfo(tenantName)
+
+    if info and 'accessToken' in info:
+        cipher = AESCipher(CIPHER_KEY)
+        return cipher.decrypt(info['accessToken'])
+
+    return None
+
+def getTenantAppId(tenantName):
+    """ get access token from db
+        return
+            1. access token string (already decrypted)
+            2. None, if not exist"""
+    info = _getTenantInfo(tenantName)
+
+    if info and 'appId' in info:
+        cipher = AESCipher(CIPHER_KEY)
+        return cipher.decrypt(info['appId'])
+
+    return None
+
+def getTenantAppSercret(tenantName):
+    """ get access token from db
+        return
+            1. access token string (already decrypted)
+            2. None, if not exist"""
+    info = _getTenantInfo(tenantName)
+
+    if info and 'appSercret' in info:
+        cipher = AESCipher(CIPHER_KEY)
+        return cipher.decrypt(info['appSercret'])
+
+    return None
+
+def getTenantUser(tenantName, userId):
+    doc = None
+
+    if userId:
+        doc = dataService.getDocument('users', tenantName, userId=userId)
+    else:
+        doc = dataService.getAllDocuments('users', tenantName)
+
+    return doc
